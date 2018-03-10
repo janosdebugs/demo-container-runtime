4d4400db7a1a5267129472a582fd5d35b3d5a5ef
János Pásztor <janos@pasztor.at>
2018-03-11 00:07:26 +0100

    Added capabilities demo

---
+++ demo/capabilities/bash-with-no-caps.cpp
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cap-ng.h>
+#include <grp.h>
+#include <sys/prctl.h>
+
+void error(const char * message) {
+    perror(message);
+    exit(-1);
+}
+
+int main() {
+    capng_clear(CAPNG_SELECT_BOTH);
+    if (capng_apply(CAPNG_SELECT_BOTH))
+        error("Failed to apply capabilities");
+
+    char bashPath[] = "/bin/bash";
+    char *const paramList[] = {bashPath};
+    if (execv(paramList[0], paramList) != 0) {
+        perror("Failed to execute bash.");
+        exit(EXIT_FAILURE);
+    }
+}
