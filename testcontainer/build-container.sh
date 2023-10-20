ae39c63ead0552f0dee80560472e0943fee0f9fe
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2023-10-20 16:30:46 +0200

    License

--- testcontainer/build-container.sh
+++ testcontainer/build-container.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-dd if=/dev/zero of=container.img  bs=10M  count=1
+wget -c https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.4-x86_64.tar.gz
+dd if=/dev/zero of=container.img  bs=50M  count=1
 mkdir -p build
 mkfs.ext4 container.img
 mount container.img build
 cd build
-tar -xvvzf ../alpine-minirootfs-3.7.0-x86_64.tar.gz
+tar -xvvzf ../alpine-minirootfs-3.18.4-x86_64.tar.gz
 cd ..
 umount build
 rm -rf build
